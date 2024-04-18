36b9e21e53c4295a289e39c2eff5b4fbbd980e5c
thedevbhardwaj <100034055+thedevbhardwaj@users.noreply.github.com>
2024-04-18 11:32:30 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser so give error but on brower it will be executed
+
+console.log(3+3) // code readability should be high
+
+console.log("Dev")
+
+
+let name = "Dev"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//different dataypes:- 
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
